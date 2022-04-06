469876e3cf3558a7b0b7d95114f4143951fcf9f8
kaurjaimeet <103113146+kaurjaimeet@users.noreply.github.com>
2022-04-06 16:21:23 +0530

    Add files via upload

---
+++ join.php
@@ -0,0 +1,22 @@
+ID 
+SSR nr. 
+SSR type 
+SSR size 
+start 
+end
+ FORWARD PRIMER1 (5'-3')
+ Tm(�C) 
+size 
+REVERSE PRIMER1 (5'-3') 
+Tm(�C) 
+size 
+PRODUCT1 size (bp) 
+start (bp) 
+end (bp) 
+FORWARD PRIMER2 (5'-3') 
+Tm(�C) size 
+REVERSE PRIMER2 (5'-3')
+ Tm(�C) size PRODUCT2 size (bp) 
+start (bp) end (bp)
+ FORWARD PRIMER3 (5'-3') Tm(�C) size REVERSE PRIMER3 (5'-3') Tm(�C) size PRODUCT3 size (bp) start (bp) end (bp) 
+
